91ae6b6524266ad1d52c71abc2d2c0c5b0b0cd95
George Borsato <georginhoborsato@gmail.com>
2022-05-17 10:58:18 -0300

    refactor: change params method

--- plus_minux.py
+++ plus_minux.py
@@ -1,20 +1,18 @@
-def plusMinus(arr):
-    pos = 0
-    neg = 0
-    zero = 0
+def plusMinus(arr, positive, negative, zero):
     for value in arr:
         if value > 0:
-            pos += 1
+            positive += 1
         elif value < 0:
-            neg += 1
+            negative += 1
         elif value == 0:
             zero += 1
     
-    print(float(pos) / float(len(arr)))
-    print(float(neg) / float(len(arr)))
+    print(float(positive) / float(len(arr)))
+    print(float(negative) / float(len(arr)))
     print(float(zero) / float(len(arr)))
     
 arr = [1,1,0, -1, -1]
 arr2 = [-4, 3, -9, 0, 4, 1]
-plusMinus(arr)
-plusMinus(arr2)
+
+plusMinus(arr, 0, 0, 0)
+plusMinus(arr2, 0, 0, 0)
